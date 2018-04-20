4b001a2c403a2fc8eb7d16a3e0f1b953457a8a52
AndRogJava <rognetta.andrea@yahoo.com>
2018-04-20 16:54:01 +0200

    Andrea Rognetta set level all at loggingTest

---
+++ Logger/src/LoggingTest.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.*;
+import java.util.logging.*;
+
+public class LoggingTest {
+
+	private static Logger log= Logger.getLogger("com.NomeProgetto");
+	private static FileHandler fh;;
+	public static void main(String[] args) throws SecurityException, IOException {
+		// TODO Auto-generated method stub
+
+		 fh= new FileHandler("myLog.txt");
+		 log.addHandler(fh);
+		 
+		 log.setLevel(Level.);
+		 
+		 log.info("First log file!");
+		 
+	}
+
+}
