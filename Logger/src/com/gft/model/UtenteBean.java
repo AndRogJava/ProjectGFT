756cc3511c3a1968aec1990abdf21983f031d423
Zacarias Chironda <Zacarias Chironda@ZACARIASCHIROND>
2018-05-21 10:39:52 +0200

    Modifica001  21/0572018 Zacarias

---
+++ Logger/src/com/gft/model/UtenteBean.java
@@ -0,0 +1,15 @@
+package com.gft.model;
+
+public class UtenteBean {
+ 
+	public static void main(String[] args) {
+	 String nome="Progetto";
+	  String cognome="GFT";
+	  
+	  System.out.println(nome + cognome);
+	  
+	  
+	   
+	}
+	 
+}
