d13a18ee91794f365e3470acb77b4a47c1972cef
ARDAVAN <ARDAVAN@192.168.0.153>
2018-05-21 11:40:18 +0200

    Ardavan 21-05-2018 generate get/set del email e toString

--- Logger/src/com/gft/model/UtenteBean.java
+++ Logger/src/com/gft/model/UtenteBean.java
@@ -13,7 +13,14 @@ public class UtenteBean {
 
 	 String nome ;
 	  String cognome;
+	  String email;
 
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	protected String getNome() {
 		return nome;
 	}
