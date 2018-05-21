99ca46dc005e70ac912b91edb6999af914d63594
Zacarias Chironda <Zacarias Chironda@ZACARIASCHIROND>
2018-05-21 10:47:11 +0200

    Aggiunta get/set... Zacarias

--- Logger/src/com/gft/model/UtenteBean.java
+++ Logger/src/com/gft/model/UtenteBean.java
@@ -1,15 +1,31 @@
 package com.gft.model;
 
 public class UtenteBean {
- 
+	 String nome ;
+	  String cognome;
+
+	protected String getNome() {
+		return nome;
+	}
+	protected void setNome(String nome) {
+		this.nome = nome;
+	}
+	protected String getCognome() {
+		return cognome;
+	}
+	protected void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
 	public static void main(String[] args) {
-	 String nome="Progetto";
-	  String cognome="GFT";
-	  
-	  System.out.println(nome + cognome);
-	  
-	  
-	   
+		 
+		UtenteBean utente = new UtenteBean();
+		utente.setNome("Progetto");
+		utente.setCognome("GFT");
+		  System.out.println(utente.getNome()+ utente.getCognome());
+		  
+		  
+		   
 	}
 	 
 }
