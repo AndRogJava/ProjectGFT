93c8cef6105b1720407d4a49c0ec5da1c1f58679
ARDAVAN <ARDAVAN@192.168.0.153>
2018-05-21 10:56:01 +0200

    Ardavan 21/05/2018 get/set Eta

--- Logger/src/com/gft/model/UtenteBean.java
+++ Logger/src/com/gft/model/UtenteBean.java
@@ -1,7 +1,14 @@
 package com.gft.model;
 
 public class UtenteBean {
+	int eta;
  
+	public int getEta() {
+		return eta;
+	}
+	public void setEta(int eta) {
+		this.eta = eta;
+	}
 	public static void main(String[] args) {
 	 String nome="Progetto";
 	  String cognome="GFT";
@@ -9,7 +16,9 @@ public class UtenteBean {
 	  System.out.println(nome + cognome);
 	  
 	  
-	   
+	  }
+	public UtenteBean(int eta) {
+		this.eta=eta;
 	}
 	 
 }
